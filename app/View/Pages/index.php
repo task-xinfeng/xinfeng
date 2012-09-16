1ff04b6f31dfd60d661234ca0b179a22790238e4
stephenday <hongyi.dai@gmail.com>
2012-09-16 16:15:42 +0800

    推荐品牌

--- app/View/Pages/index.php
+++ app/View/Pages/index.php
@@ -1,7 +1,85 @@
 <div class="content block_center">
 
 <!--供应商-->
-<div class="block_center thumb_list_block brand clearfix"></div>
+    <div class="block_center thumb_list_block thumb_list_block_opposite brand clearfix">
+    	<div class="top_title">
+            <h2>品牌推荐</h2>
+        </div>
+        <div class="main">
+        	<ul class="items_group clearfix">
+            	<li class="item">
+                	<div class="pic">
+                    	<a href="#">
+                        	<img src="#" width="165" height="100" />
+                        </a>
+                    </div>
+                    <h3><a href="#">title</a></h3>
+                </li>
+                <li class="item">
+                	<div class="pic">
+                    	<a href="#">
+                        	<img src="#" width="165" height="100" />
+                        </a>
+                    </div>
+                    <h3><a href="#">title</a></h3>
+                </li>
+                <li class="item">
+                	<div class="pic">
+                    	<a href="#">
+                        	<img src="#" width="165" height="100" />
+                        </a>
+                    </div>
+                    <h3><a href="#">title</a></h3>
+                </li>
+                <li class="item">
+                	<div class="pic">
+                    	<a href="#">
+                        	<img src="#" width="165" height="100" />
+                        </a>
+                    </div>
+                    <h3><a href="#">title</a></h3>
+                </li>
+                <li class="item">
+                	<div class="pic">
+                    	<a href="#">
+                        	<img src="#" width="165" height="100" />
+                        </a>
+                    </div>
+                    <h3><a href="#">title</a></h3>
+                </li>
+                <li class="item">
+                	<div class="pic">
+                    	<a href="#">
+                        	<img src="#" width="165" height="100" />
+                        </a>
+                    </div>
+                    <h3><a href="#">title</a></h3>
+                </li>
+                <li class="item">
+                	<div class="pic">
+                    	<a href="#">
+                        	<img src="#" width="165" height="100" />
+                        </a>
+                    </div>
+                    <h3><a href="#">title</a></h3>
+                </li>
+                <li class="item more">
+                	<a href="#" target="_blank" title="更多">
+                        <span class="more_icon"></span>
+                        <span class="more_text">更多</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="side">
+        	<div class="a a_b">
+            	<a href="#" target="_blank">
+                	<img src="#" width="238" height="298" />
+                </a>
+            </div>
+        </div>
+
+    </div>
 <!--供应商 ends-->
 
 <!--通栏广告-->
@@ -171,6 +249,14 @@
     </a>
 </div>
 <!--通栏广告 ends-->
+
+<!--供应商-->
+    <div class="block_center news_list_block news clearfix">
+    	<div class="top_title">
+            <h2>行业新闻</h2>
+        </div>
+    </div>
+<!--供应商 ends-->
 
 
 </div>
