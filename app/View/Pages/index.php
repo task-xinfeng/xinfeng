301d622676d445997c26c997a61a572e9f4e3f74
stephenday <hongyi.dai@gmail.com>
2012-09-19 22:11:01 +0800

    intro_bottom_ul

--- app/View/Pages/index.php
+++ app/View/Pages/index.php
@@ -124,7 +124,7 @@
 </div>
 <!--通栏广告 ends-->
 
-<!--新风供应商-->
+<!--新风品牌-->
 	<div class="block_center thumb_list_block suppliers clearfix">
     	<div class="top_title">
             <h2>新风品牌</h2>
@@ -194,7 +194,54 @@
             </div>
         </div>
 	</div>
-<!--新风供应商 ends-->
+<!--新风品牌 ends-->
+
+<!--通栏广告-->
+<div class="a_w block_center">
+	<a href="#">
+    	<img width="990" height="80" src="#" />
+    </a>
+</div>
+<!--通栏广告 ends-->
+
+<!--行业新闻-->
+    <div class="block_center news_list_block news clearfix">
+    	<div class="top_title">
+            <h2>行业新闻</h2>
+        </div>
+        <div class="main">
+        	<ul class="items_group clearfix">
+            	<li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item"><a href="#">东风率先整合自主品牌 三大品牌集体换标</a><span class="date">2012-12-02</span></li>
+                <li class="item more"><a href="#">查看更多</a></li>
+            </ul>
+        </div>
+        <div class="side">
+        	<div class="a a_b">
+            	<a href="#" target="_blank">
+                	<img src="#" width="238" height="298" />
+                </a>
+            </div>
+        </div>
+    </div>
+<!--行业新闻 ends-->
 
 <!--通栏广告-->
 <div class="a_w block_center">
@@ -284,10 +331,10 @@
 </div>
 <!--通栏广告 ends-->
 
-<!--行业新闻-->
-    <div class="block_center news_list_block news clearfix">
+<!--新风导购-->
+    <div class="block_center news_list_block guide clearfix">
     	<div class="top_title">
-            <h2>行业新闻</h2>
+            <h2>新风导购</h2>
         </div>
         <div class="main">
         	<ul class="items_group clearfix">
@@ -321,11 +368,32 @@
             </div>
         </div>
     </div>
-<!--行业新闻 ends-->
+<!--新风导购 ends-->
 
 <!--底部介绍-->
 	<div class="block_center intro_bottom">
-    
+    	<ul class="intro_ul">
+        	<li class="i i_1">
+            	<span class="icon"></span>
+                <p>最权威的新风行业动态</p>
+                <em>汇聚全国数百家新风厂商第一手的新闻资料，聚合全网最新最全的新风行业信息。</em>
+            </li>
+            <li class="i i_2">
+            	<span class="icon"></span>
+                <p>找到一切你想找的</p>
+                <em>在新风社，你可以搜索到最新最全的新风行业的产品、新闻、厂商、经销商。</em>
+            </li>
+            <li class="i i_3">
+            	<span class="icon"></span>
+                <p>零距离接近消费群</p>
+                <em>在新风社，厂商、经销商将和消费者史无前例地零距离接触。</em>
+            </li>
+            <li class="i i_4">
+            	<span class="icon"></span>
+                <p>传播你的品牌</p>
+                <em>通过搜索引擎营销、微博营销、数据库营销，为你打造一个立体的传播环境。</em>
+             </li>
+        </ul>
     </div>
 <!--底部介绍 ends-->
 
