b2e26c6dbdb39b32c4ca0ccab2c22db35c03006c
stephenday <hongyi.dai@gmail.com>
2012-09-23 14:30:16 +0800

    brands page

---
+++ app/View/Brands/index.php
@@ -0,0 +1,189 @@
+<div class="content block_center brands">
+<!--top 广告-->
+<div class="p_slide block_center">
+	<ul class="p_ul">
+    	<li>
+        	<a href="#"><img src="#" /></a>
+        </li>
+        <li>
+        	<a href="#"><img src="#" /></a>
+        </li>
+        <li>
+        	<a href="#"><img src="#" /></a>
+        </li>
+        <li>
+        	<a href="#"><img src="#" /></a>
+        </li>
+        <li>
+        	<a href="#"><img src="#" /></a>
+        </li>
+    </ul>
+    <ul class="c_ul clearfix">
+    	<li>1</li>
+        <li class="current">2</li>
+        <li>3</li>
+        <li>4</li>
+    </ul>
+</div>
+<!--top 广告 ends-->
+
+<!--品牌缩略图列表-->
+<div class="block_center thumb_320_block">
+	<ul class="thumb_320_ul clearfix">
+    	<li>
+        	<h2><a href="#">松下</a></h2>
+            <div class="pic"><a href="#"><img src="#" width="320" height="190"/></a></div>
+            <div class="figures clearfix">
+            	<div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+            </div>
+            <div class="shadow"></div>
+        </li>
+		<li>
+        	<h2><a href="#">松下</a></h2>
+            <div class="pic"><a href="#"><img src="#" width="320" height="190"/></a></div>
+            <div class="figures clearfix">
+            	<div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+            </div>
+            <div class="shadow"></div>
+        </li>
+        <li class="right">
+        	<h2><a href="#">松下</a></h2>
+            <div class="pic"><a href="#"><img src="#" width="320" height="190"/></a></div>
+            <div class="figures clearfix">
+            	<div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+            </div>
+            <div class="shadow"></div>
+        </li>
+        <li>
+        	<h2><a href="#">松下</a></h2>
+            <div class="pic"><a href="#"><img src="#" width="320" height="190"/></a></div>
+            <div class="figures clearfix">
+            	<div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+            </div>
+            <div class="shadow"></div>
+        </li>
+        <li>
+        	<h2><a href="#">松下</a></h2>
+            <div class="pic"><a href="#"><img src="#" width="320" height="190"/></a></div>
+            <div class="figures clearfix">
+            	<div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+            </div>
+            <div class="shadow"></div>
+        </li>
+        <li class="right">
+        	<h2><a href="#">松下</a></h2>
+            <div class="pic"><a href="#"><img src="#" width="320" height="190"/></a></div>
+            <div class="figures clearfix">
+            	<div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+            </div>
+            <div class="shadow"></div>
+        </li>
+        <li>
+        	<h2><a href="#">松下</a></h2>
+            <div class="pic"><a href="#"><img src="#" width="320" height="190"/></a></div>
+            <div class="figures clearfix">
+            	<div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+            </div>
+            <div class="shadow"></div>
+        </li>
+        <li>
+        	<h2><a href="#">松下</a></h2>
+            <div class="pic"><a href="#"><img src="#" width="320" height="190"/></a></div>
+            <div class="figures clearfix">
+            	<div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+                <div class="num_item">
+                	<span class="num"><a href="#">34</a></span>
+                    <div class="t">新风产品</div>
+                </div>
+            </div>
+            <div class="shadow"></div>
+        </li>
+    </ul>
+</div>
+<!--品牌缩略图列表 ends-->
+
+</div>
