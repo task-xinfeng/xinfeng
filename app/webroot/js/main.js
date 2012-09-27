a788193cfcd0421107e6cb271222a9f840ad4d1a
stephenday <hongyi.dai@gmail.com>
2012-09-28 00:02:32 +0800

    about交互

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -3,4 +3,16 @@ $(document).ready(function(){
 	$('.thumb_list_block .main .items_group li.more a').hover(function(){
 		$(this).find('span.more_icon').toggleClass('more_icon_hover');
 	});
+	
+	$('.about_menu li a').click(function(event){
+		event.preventDefault();
+		//alert('done');
+		if(!$(this).hasClass('current')){
+			var x = $(this).parent().siblings().andSelf().index($(this).parent());
+			//alert(x);
+			$(this).parent().siblings().find('a').removeClass('current').end().end().end().addClass('current');
+			$('.about_detail').hide().eq(x).fadeIn(500);
+		}
+	});
+	
 });
