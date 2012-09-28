d6a972bca714e7d1822e048b72283ff6e9f462b6
task-xinfeng <task-xinfeng@gmail.com>
2012-09-28 22:16:22 +0800

    更新footer

--- app/View/Elements/classic_footer.php
+++ app/View/Elements/classic_footer.php
@@ -1,6 +1,19 @@
 <div class="footer_w">
   <div class="block_center footer">
-    <div class="links"> <a href="?about_index=0">关于我们</a> <a href="?about_index=1">免责声明</a> <a href="?about_index=2">商务合作</a> <a href="?about_index=3">联系方式</a> </div>
+    <div class="links"> 
+		<a href="<?php echo $this->Html->url(array("controller" => "pages","action" => "about","?"=>array("about_index" => 0)));?>">
+			关于我们
+		</a> 
+		<a href="<?php echo $this->Html->url(array("controller" => "pages","action" => "about","?"=>array("about_index" => 1)));?>">
+			免责声明
+		</a> 
+		<a href="<?php echo $this->Html->url(array("controller" => "pages","action" => "about","?"=>array("about_index" => 2)));?>">
+			商务合作
+		</a> 
+		<a href="<?php echo $this->Html->url(array("controller" => "pages","action" => "about","?"=>array("about_index" => 3)));?>">
+			联系方式
+		</a> 
+	</div>
     <div class="copyright"></div>
   </div>
 </div>
