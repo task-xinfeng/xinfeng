47445320f866200cbc5e4b5c2583acf30f6679d5
task-xinfeng <task-xinfeng@gmail.com>
2012-09-28 22:08:28 +0800

    Revert "ci"
    This reverts commit e90d7edadbacef862e2060dcce4aaab78fe7a131.

--- app/View/Elements/classic_footer.php
+++ app/View/Elements/classic_footer.php
@@ -1,19 +1,6 @@
 <div class="footer_w">
   <div class="block_center footer">
-    <div class="links"> 
-		<a href="<?php echo $this->Html->url(array("controller" => "pages","action" => "about"));?>">
-			关于我们
-		</a> 
-		<a href="<?php echo $this->Html->url(array("controller" => "pages","action" => "about"));?>">
-			免责声明
-		</a>
-		<a href="<?php echo $this->Html->url(array("controller" => "pages","action" => "about"));?>">
-			商务合作
-		</a> 
-		<a href="<?php echo $this->Html->url(array("controller" => "pages","action" => "about"));?>">
-			联系方式
-		</a> 
-	</div>
+    <div class="links"> <a href="#">关于我们</a> <a href="#">免责声明</a> <a href="#">商务合作</a> <a href="#">联系方式</a> </div>
     <div class="copyright"></div>
   </div>
 </div>
