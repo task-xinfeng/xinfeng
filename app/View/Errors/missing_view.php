25c1ddeda05ceddc40beed477bce1ee0ee693f47
task-xinfeng <task-xinfeng@gmail.com>
2012-10-09 22:39:08 +0800

    错误页面

--- app/View/Errors/missing_view.php
+++ app/View/Errors/missing_view.php
@@ -2,6 +2,6 @@
 	<div class="error_page_icon"></div>
     <div class="error_tip">
     	<p class="a">您所访问的页面<span>不存在</span>或者<span>已被删除</span> <?php //echo $name?></p>
-        <p class="b"><a href="#">返回首页</a></p>
+        <p class="b"><a href="<?php echo $this->webroot;?>">返回首页</a></p>
     </div>
 </div>
